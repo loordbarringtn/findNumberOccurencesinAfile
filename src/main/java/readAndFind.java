0883a8473a8d94af7f0452005f173a963384f14d
lordbarrington <quantas@gmail.com>
2021-03-16 00:02:17 +0300

    code for new task added

--- src/main/java/readAndFind.java
+++ src/main/java/readAndFind.java
@@ -6,21 +6,38 @@ import java.util.Scanner;
 public class readAndFind {
     public static void main(String[] args) throws FileNotFoundException {
 
-        ArrayList<Integer> result = new ArrayList<>();
+        ArrayList<Integer> numbersFromFileOne = new ArrayList<>();
+        ArrayList<Integer> numbersFromFileTwo = new ArrayList<>();
         int counter = 0;
+        int max = Integer.MIN_VALUE;
 
         try (Scanner s = new Scanner(new FileReader("src/main/resources/dataset_91022.txt"))) {
             while (s.hasNext()) {
-                result.add(s.nextInt());
+                numbersFromFileOne.add(s.nextInt());
             }
         }
 
-        for (int i = 0; i< result.size(); i++){
-            if (result.get(i)>=9999){
+        for (int i = 0; i< numbersFromFileOne.size(); i++){
+            if (numbersFromFileOne.get(i)>=9999){
                 counter++;
             }
         }
 
         System.out.println("Total occurrences: " + counter);
+
+        try (Scanner s = new Scanner(new FileReader("src/main/resources/dataset_91007.txt"))) {
+            while (s.hasNext()) {
+                numbersFromFileTwo.add(s.nextInt());
+            }
+        }
+
+        for (int i = 0; i< numbersFromFileTwo.size(); i++){
+            if (numbersFromFileTwo.get(i) > max) {
+                max = numbersFromFileTwo.get(i);
+            }
+        }
+
+        System.out.println("Largest number:" + max);
+
     }
 }
